df55226bb76a782942353e73476563ed3ba24d9c
KirillDemyanenko <demyanenkokg@gmail.com>
2023-07-26 14:59:24 +0300

    feat: add swagger

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,45 +1,7 @@
-import {
-  Body,
-  Controller,
-  Delete,
-  Get,
-  Param,
-  Post,
-  Put,
-} from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 import { AppService } from './app.service';
-import UserEntity from './entities/user.entity';
-import { InMemoryDBService } from '@nestjs-addons/in-memory-db';
 
 @Controller()
 export class AppController {
-  constructor(
-    private readonly appService: AppService,
-    private userService: InMemoryDBService<UserEntity>,
-  ) {}
-
-  @Get('/user')
-  getUsers() {
-    return this.userService.getAll();
-  }
-
-  @Get('/user/:id')
-  GetUserById(@Param('id') id: string) {
-    return this.userService.query((data) => data.id === id);
-  }
-
-  @Post('/user')
-  addUser(@Body() user: UserEntity): UserEntity {
-    return this.userService.create(user);
-  }
-
-  @Put('/user')
-  EditUser(@Body() user: UserEntity) {
-    return this.userService.update(user);
-  }
-
-  @Delete('/user/:id')
-  DeleteUser(@Param('id') id: string) {
-    return this.userService.delete(id);
-  }
+  constructor(private readonly appService: AppService) {}
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule } from '@nestjs/swagger';
+import * as fs from 'fs';
+import * as YAML from 'js-yaml';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const yamlFile = fs.readFileSync('doc/api.yaml', 'utf8');
+  const document = SwaggerModule.createDocument(app, YAML.load(yamlFile));
+  SwaggerModule.setup('doc', app, document);
   await app.listen(4000);
 }
 bootstrap();
