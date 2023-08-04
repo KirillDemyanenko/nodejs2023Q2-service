dbf1011fe09935519cfe3607ac953fa681e175c1
KirillDemyanenko <demyanenkokg@gmail.com>
2023-08-04 14:55:16 +0300

    feat: import typeorm module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,12 +5,29 @@ import { TrackController } from './track/track.controller';
 import { UserController } from './user/user.controller';
 import { InMemoryDBService } from '@nestjs-addons/in-memory-db';
 import { ArtistController } from './artist/artist.controller';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AlbumController } from './album/album.controller';
 import { FavoritesController } from './favorites/favorites.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DB_HOST'),
+        port: +configService.get<number>('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_NAME'),
+        entities: [],
+        synchronize: true,
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [
     AppController,
     UserController,
