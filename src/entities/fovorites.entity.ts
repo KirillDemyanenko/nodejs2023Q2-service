52533effd970a3559e810d6314b124bbc7161a50
KirillDemyanenko <demyanenkokg@gmail.com>
2023-08-09 13:45:32 +0300

    refactor: temporary comment unused code

--- src/entities/fovorites.entity.ts
+++ src/entities/fovorites.entity.ts
@@ -1,5 +1,28 @@
-export default interface FavoritesEntity {
-  artists: string[];
-  albums: string[];
-  tracks: string[];
-}
+// export default interface FavoritesEntity {
+//   artists: string[];
+//   albums: string[];
+//   tracks: string[];
+// }
+
+// import { Column, Entity } from 'typeorm';
+//
+// @Entity()
+// export class FavoritesEntity {
+//   @Column({
+//     nullable: false,
+//     default: '',
+//   })
+//   artists: string;
+//
+//   @Column({
+//     nullable: false,
+//     default: '',
+//   })
+//   namalbums: string;
+//
+//   @Column({
+//     nullable: false,
+//     default: false,
+//   })
+//   grammy: boolean;
+// }
