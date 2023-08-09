fff99ce9ae9ab6deef6240d22d81d2d4cfd03df3
KirillDemyanenko <demyanenkokg@gmail.com>
2023-08-09 13:45:02 +0300

    feat: remade entity user to work with typeorm

--- src/entities/user.entity.ts
+++ src/entities/user.entity.ts
@@ -1,9 +1,45 @@
 import { InMemoryDBEntity } from '@nestjs-addons/in-memory-db';
 
-export default interface UserEntity extends InMemoryDBEntity {
+// export default interface Users extends InMemoryDBEntity {
+//   login: string;
+//   password: string;
+//   version: number;
+//   createdAt: number;
+//   updatedAt: number;
+// }
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity()
+export default class Users {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({
+    nullable: false,
+    default: '',
+  })
   login: string;
+
+  @Column({
+    nullable: false,
+    default: '',
+  })
   password: string;
+
+  @Column({
+    nullable: false,
+    default: 1,
+  })
   version: number;
+
+  @CreateDateColumn()
   createdAt: number;
+
+  @CreateDateColumn()
   updatedAt: number;
 }
