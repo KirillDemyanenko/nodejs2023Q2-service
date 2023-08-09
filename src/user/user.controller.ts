fb8a45bec11716f66874f32d8ae64e1ec7fca7f9
KirillDemyanenko <demyanenkokg@gmail.com>
2023-08-09 16:50:49 +0300

    feat: add handler for getUsers, getUserById

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -16,78 +16,84 @@ import CreateUserDto from '../interfaces/createUserDTO';
 import { isUUID } from 'class-validator';
 import UpdatePasswordDto from '../interfaces/updatePasswordDto';
 import { AppService } from '../app.service';
+import { InjectDataSource } from '@nestjs/typeorm';
+import { DataSource } from 'typeorm';
+import Artists from '../entities/artist.entity';
 
 @Controller('user')
 export class UserController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    @InjectDataSource('database')
+    private dataSource: DataSource,
+  ) {}
 
   @Get()
-  getUsers(): Users[] {
-    return this.appService.userService.getAll();
+  async getUsers(): Promise<Users[]> {
+    return await this.dataSource.manager.find(Users);
   }
 
   @Get(':id')
-  getUserById(@Param('id') id: string): Users[] {
+  async getUserById(@Param('id') id: string): Promise<Users> {
     if (!isUUID(id, 4)) throw new BadRequestException('Invalid user id');
-    if (!this.appService.userService.get(id))
-      throw new NotFoundException(`User with id - ${id} not found!`);
-    return this.appService.userService.query((data) => data.id === id);
+    const user = await this.dataSource.manager.findOneBy(Users, { id: id });
+    if (!user) throw new NotFoundException(`User with id - ${id} not found!`);
+    return user;
   }
-
-  @Post()
-  addUser(@Body() user: CreateUserDto): Partial<Users> {
-    if (!user.password || !user.login)
-      throw new BadRequestException('Body does not contain required fields');
-    const newUser: Users = {
-      id: '',
-      password: user.password,
-      login: user.login,
-      createdAt: Date.now(),
-      version: 1,
-      updatedAt: Date.now(),
-    };
-    const { id, login, createdAt, version, updatedAt } =
-      this.appService.userService.create(newUser);
-    return {
-      id: id,
-      login: login,
-      createdAt: createdAt,
-      version: version,
-      updatedAt: updatedAt,
-    };
-  }
-
-  @Put(':id')
-  editUser(@Param('id') idRea: string, @Body() passwords: UpdatePasswordDto) {
-    if (!isUUID(idRea, 4)) throw new BadRequestException('Invalid user id');
-    if (!passwords.newPassword || !passwords.oldPassword)
-      throw new BadRequestException('Body does not contain required fields');
-    const user: Users = this.appService.userService.get(idRea);
-    if (!user)
-      throw new NotFoundException(`User with id - ${idRea} not found!`);
-    if (user.password !== passwords.oldPassword)
-      throw new ForbiddenException(`Wrong old password!`);
-    user.password = passwords.newPassword;
-    user.version = user.version + 1;
-    user.updatedAt = Date.now();
-    this.appService.userService.update(user);
-    const { id, login, createdAt, version, updatedAt } =
-      this.appService.userService.get(idRea);
-    return {
-      id: id,
-      login: login,
-      createdAt: createdAt,
-      version: version,
-      updatedAt: updatedAt,
-    };
-  }
-
-  @HttpCode(204)
-  @Delete(':id')
-  deleteUser(@Param('id') id: string): void {
-    if (!isUUID(id, 4)) throw new BadRequestException('Invalid user id');
-    if (!this.appService.userService.get(id))
-      throw new NotFoundException(`User with id - ${id} not found!`);
-    return this.appService.userService.delete(id);
-  }
+  //
+  // @Post()
+  // addUser(@Body() user: CreateUserDto): Partial<Users> {
+  //   if (!user.password || !user.login)
+  //     throw new BadRequestException('Body does not contain required fields');
+  //   const newUser: Users = {
+  //     id: '',
+  //     password: user.password,
+  //     login: user.login,
+  //     createdAt: Date.now(),
+  //     version: 1,
+  //     updatedAt: Date.now(),
+  //   };
+  //   const { id, login, createdAt, version, updatedAt } =
+  //     this.appService.userService.create(newUser);
+  //   return {
+  //     id: id,
+  //     login: login,
+  //     createdAt: createdAt,
+  //     version: version,
+  //     updatedAt: updatedAt,
+  //   };
+  // }
+  //
+  // @Put(':id')
+  // editUser(@Param('id') idRea: string, @Body() passwords: UpdatePasswordDto) {
+  //   if (!isUUID(idRea, 4)) throw new BadRequestException('Invalid user id');
+  //   if (!passwords.newPassword || !passwords.oldPassword)
+  //     throw new BadRequestException('Body does not contain required fields');
+  //   const user: Users = this.appService.userService.get(idRea);
+  //   if (!user)
+  //     throw new NotFoundException(`User with id - ${idRea} not found!`);
+  //   if (user.password !== passwords.oldPassword)
+  //     throw new ForbiddenException(`Wrong old password!`);
+  //   user.password = passwords.newPassword;
+  //   user.version = user.version + 1;
+  //   user.updatedAt = Date.now();
+  //   this.appService.userService.update(user);
+  //   const { id, login, createdAt, version, updatedAt } =
+  //     this.appService.userService.get(idRea);
+  //   return {
+  //     id: id,
+  //     login: login,
+  //     createdAt: createdAt,
+  //     version: version,
+  //     updatedAt: updatedAt,
+  //   };
+  // }
+  //
+  // @HttpCode(204)
+  // @Delete(':id')
+  // deleteUser(@Param('id') id: string): void {
+  //   if (!isUUID(id, 4)) throw new BadRequestException('Invalid user id');
+  //   if (!this.appService.userService.get(id))
+  //     throw new NotFoundException(`User with id - ${id} not found!`);
+  //   return this.appService.userService.delete(id);
+  // }
 }
