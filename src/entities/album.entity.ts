4804fb5e40a250474d609864e932ea0592866655
KirillDemyanenko <demyanenkokg@gmail.com>
2023-08-09 13:44:23 +0300

    feat: remade entity album to work with typeorm

--- src/entities/album.entity.ts
+++ src/entities/album.entity.ts
@@ -1,7 +1,33 @@
-import { InMemoryDBEntity } from '@nestjs-addons/in-memory-db';
+// import { InMemoryDBEntity } from '@nestjs-addons/in-memory-db';
 
-export default interface AlbumEntity extends InMemoryDBEntity {
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export default class Albums {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({
+    nullable: false,
+    default: '',
+  })
   name: string;
+
+  @Column({
+    nullable: false,
+    default: '',
+  })
   year: string;
+
+  @Column({
+    nullable: true,
+    default: null,
+  })
   artistId: string | null;
 }
+
+// export default interface Albums extends InMemoryDBEntity {
+//   name: string;
+//   year: string;
+//   artistId: string | null;
+// }
