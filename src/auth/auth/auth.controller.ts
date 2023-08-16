8c10beeead91cecdb183ef186f1d08d1bcf998d7
KirillDemyanenko <demyanenkokg@gmail.com>
2023-08-16 15:09:12 +0300

    feat: add endpoints for authorization

--- src/auth/auth/auth.controller.ts
+++ src/auth/auth/auth.controller.ts
@@ -1,11 +1,11 @@
-import { BadRequestException, Body, Controller, HttpCode, Post } from "@nestjs/common";
+import { BadRequestException, Body, Controller, HttpCode, Post, UnauthorizedException } from "@nestjs/common";
 import CreateUserDto from '../../interfaces/createUserDTO';
 
 @Controller('auth')
 export class AuthController {
   @HttpCode(201)
   @Post('/signup')
-  async addNewUser(@Body() user: CreateUserDto): Promise<string> {
+  async signUp(@Body() user: CreateUserDto): Promise<string> {
     if (
       !user.password ||
       !user.login ||
@@ -13,6 +13,27 @@ export class AuthController {
       typeof user.password !== 'string'
     )
       throw new BadRequestException('DTO is invalid');
+    return 'Valid'
+  }
+
+  @Post('/login')
+  async login(@Body() user: CreateUserDto): Promise<string> {
+    if (
+      !user.password ||
+      !user.login ||
+      typeof user.login !== 'string' ||
+      typeof user.password !== 'string'
+    )
+      throw new BadRequestException('DTO is invalid');
+    return 'Valid'
+  }
+
+  @Post('/refresh')
+  async refresh(@Body() token: {refreshToken: string}): Promise<string> {
+    if (
+      !token.refreshToken || typeof token.refreshToken !== "string"
+    )
+      throw new UnauthorizedException('DTO is invalid');
     return 'Valid'
   }
 }
