b7b56ec8772b184144630073dd86de64c6a4d3ac
KirillDemyanenko <demyanenkokg@gmail.com>
2023-08-17 22:04:45 +0300

    fix: guard token verification

--- src/auth/auth.guard.ts
+++ src/auth/auth.guard.ts
@@ -1,17 +1,33 @@
-import { CanActivate, ExecutionContext, Injectable, UnauthorizedException } from "@nestjs/common";
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { Observable } from 'rxjs';
-import { JwtService } from "@nestjs/jwt";
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-  constructor(private jwtService: JwtService) {
-
-  }
+  constructor(
+    private jwtService: JwtService,
+    private configService: ConfigService,
+  ) {}
   canActivate(
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
-    const request = context.switchToHttp().getRequest()
-    if (!request.headers['authorization']) throw new UnauthorizedException();
+    const request = context.switchToHttp().getRequest();
+    if (!request.headers.authorization) throw new UnauthorizedException();
+    const [type, token] = request.headers.authorization.split(' ');
+    if (type !== 'Bearer') throw new UnauthorizedException('Wrong scheme!');
+    try {
+      this.jwtService.verify(token, {
+        secret: this.configService.get('JWT_SECRET'),
+      });
+    } catch (err) {
+      throw new UnauthorizedException('Token not valid!');
+    }
     return true;
   }
 }
