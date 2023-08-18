421944198489d66d118f6d565f77f5de9261e04d
KirillDemyanenko <demyanenkokg@gmail.com>
2023-08-18 11:58:10 +0300

    feat: using logger for guard

--- src/auth/auth.guard.ts
+++ src/auth/auth.guard.ts
@@ -7,27 +7,42 @@ import {
 import { Observable } from 'rxjs';
 import { JwtService } from '@nestjs/jwt';
 import { ConfigService } from '@nestjs/config';
+import { LibraryLogger } from '../logger/logger';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
   constructor(
     private jwtService: JwtService,
     private configService: ConfigService,
+    private readonly logger: LibraryLogger,
   ) {}
   canActivate(
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
     const request = context.switchToHttp().getRequest();
-    if (!request.headers.authorization) throw new UnauthorizedException();
+    const response = context.switchToHttp().getResponse();
+    if (!request.headers.authorization) {
+      this.logger.error('Authorization header missing. Access is denied.');
+      throw new UnauthorizedException();
+    }
     const [type, token] = request.headers.authorization.split(' ');
-    if (type !== 'Bearer') throw new UnauthorizedException('Wrong scheme!');
+    if (type !== 'Bearer') {
+      this.logger.error('Wrong authorization scheme used. Access is denied.');
+      throw new UnauthorizedException('Wrong scheme!');
+    }
     try {
       this.jwtService.verify(token, {
         secret: this.configService.get('JWT_SECRET'),
       });
     } catch (err) {
+      this.logger.error(
+        'An access attempt was made with an invalid token. Access is denied.',
+      );
       throw new UnauthorizedException('Token not valid!');
     }
+    this.logger.verbose(
+      `Assess to - ${request.url} - for - ${request.method} - ended with code - ${response.statusCode}`,
+    );
     return true;
   }
 }
