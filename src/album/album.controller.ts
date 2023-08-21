29774f13ffea8a110691e2d9d8cc32917becb980
KirillDemyanenko <demyanenkokg@gmail.com>
2023-08-21 13:06:34 +0300

    feat: add logs to albums controller

--- src/album/album.controller.ts
+++ src/album/album.controller.ts
@@ -8,9 +8,9 @@ import {
   NotFoundException,
   Param,
   Post,
-  Put,
-  UseGuards,
-} from '@nestjs/common';
+  Put, Req,
+  UseGuards
+} from "@nestjs/common";
 import { isUUID } from 'class-validator';
 import Albums from '../entities/album.entity';
 import { DataSource } from 'typeorm';
@@ -18,40 +18,60 @@ import { InjectDataSource } from '@nestjs/typeorm';
 import Tracks from '../entities/track.entity';
 import { FavoritesAlbums } from '../entities/fovorites.entity';
 import { AuthGuard } from '../auth/auth.guard';
+import { LibraryLogger } from "../logger/logger";
 
 @Controller('album')
 export class AlbumController {
   constructor(
     @InjectDataSource('database')
     private dataSource: DataSource,
+    private readonly logger: LibraryLogger,
   ) {}
 
   @UseGuards(AuthGuard)
   @Get()
-  async getAlbums(): Promise<Albums[]> {
+  async getAlbums(@Req() request: Request): Promise<Albums[]> {
+    this.logger.verbose(
+      `Request to - ${request.url} without query params and with body - ${JSON.stringify(request.body)}.`,
+    );
     return await this.dataSource.manager.find(Albums);
   }
 
   @UseGuards(AuthGuard)
   @Get(':id')
-  async etAlbumById(@Param('id') id: string): Promise<Albums> {
-    if (!isUUID(id, 4)) throw new BadRequestException('Invalid album id');
+  async etAlbumById(@Param('id') id: string, @Req() request: Request ): Promise<Albums> {
+    this.logger.verbose(
+      `Request to - ${request.url} with query params - id = ${id} and with body - ${JSON.stringify(request.body)}.`,
+    );
+    if (!isUUID(id, 4)) {
+      this.logger.error(`Request to - ${request.url} - Invalid album id`)
+      throw new BadRequestException("Invalid album id");
+    }
     const album = await this.dataSource.manager.findOneBy<Albums>(Albums, { id: id });
-    if (!album) throw new NotFoundException(`Album with id - ${id} not found!`);
+    if (!album) {
+      this.logger.error(`Request to - ${request.url} - Album with id - ${id} not found!`)
+      throw new NotFoundException(`Album with id - ${id} not found!`);
+    }
     return album;
   }
 
   @UseGuards(AuthGuard)
   @Post()
-  async addAlbum(@Body() album: Partial<Albums>): Promise<Albums> {
-    if (!album.name || !album.year)
-      throw new BadRequestException('Body does not contain required fields');
+  async addAlbum(@Body() album: Partial<Albums>, @Req() request: Request): Promise<Albums> {
+    this.logger.verbose(
+      `Request to - ${request.url} without query params and with body - ${JSON.stringify(request.body)}.`,
+    );
+    if (!album.name || !album.year) {
+      this.logger.error(`Request to - ${request.url} - Body does not contain required fields`)
+      throw new BadRequestException("Body does not contain required fields");
+    }
     const newAlbum: Albums = new Albums();
     newAlbum.name = album.name;
     newAlbum.year = album.year;
     newAlbum.artistId = album.artistId || null;
     const albumsEntity = this.dataSource.manager.create(Albums, newAlbum);
     await this.dataSource.manager.save(albumsEntity);
+    this.logger.log(`New album - ${JSON.stringify(albumsEntity)} successfully added`)
     return albumsEntity;
   }
 
@@ -59,36 +79,57 @@ export class AlbumController {
   @Put(':id')
   async editAlbum(
     @Param('id') id: string,
+    @Req() request: Request,
     @Body() albumInfo: Partial<Albums>,
   ): Promise<Albums> {
-    if (!isUUID(id, 4)) throw new BadRequestException('Invalid album id');
+    this.logger.verbose(
+      `Request to - ${request.url} with query params - id = ${id} and with body - ${JSON.stringify(request.body)}.`,
+    );
+    if (!isUUID(id, 4)) {
+      this.logger.error(`Request to - ${request.url} - Invalid album id`)
+      throw new BadRequestException("Invalid album id");
+    }
     if (
       !albumInfo.name ||
       !albumInfo.year ||
       !(albumInfo.artistId === null || typeof albumInfo.artistId === 'string')
-    )
-      throw new BadRequestException('Body does not contain required fields');
+    ) {
+      this.logger.error(`Request to - ${request.url} - Body does not contain required fields`)
+      throw new BadRequestException("Body does not contain required fields");
+    }
     const album: Albums = await this.dataSource.manager.findOneBy<Albums>(Albums, {
       id: id,
     });
-    if (!album) throw new NotFoundException(`Album with id - ${id} not found!`);
+    if (!album) {
+      this.logger.error(`Request to - ${request.url} - Album with id - ${id} not found!`)
+      throw new NotFoundException(`Album with id - ${id} not found!`);
+    }
     album.year = albumInfo.year || album.year;
     album.name = albumInfo.name || album.name;
     album.artistId = albumInfo.artistId || album.artistId;
     await this.dataSource.manager.save(Albums, album);
+    this.logger.log(`Album - ${JSON.stringify(album)} successfully updated!`)
     return album;
   }
 
   @UseGuards(AuthGuard)
   @HttpCode(204)
   @Delete(':id')
-  async deleteAlbum(@Param('id') id: string) {
-    if (!isUUID(id, 4)) throw new BadRequestException('Invalid album id');
+  async deleteAlbum(@Param('id') id: string, @Req() request: Request) {
+    this.logger.verbose(
+      `Request to - ${request.url} with query params - id = ${id} and with body - ${JSON.stringify(request.body)}.`,
+    );
+    if (!isUUID(id, 4)) {
+      this.logger.error(`Request to - ${request.url} - Invalid album id`)
+      throw new BadRequestException("Invalid album id");
+    }
     const albumForDelete = await this.dataSource.manager.findOneBy(Albums, {
       id: id,
     });
-    if (!albumForDelete)
+    if (!albumForDelete) {
+      this.logger.error(`Request to - ${request.url} - Album with id - ${id} not found!`)
       throw new NotFoundException(`Album with id - ${id} not found!`);
+    }
     const tracksWithAlbum = await this.dataSource.manager.findBy(Tracks, {
       albumId: id,
     });
@@ -97,6 +138,7 @@ export class AlbumController {
     const album = await this.dataSource.manager.findOneBy(FavoritesAlbums, {
       albumID: id,
     });
+    this.logger.log(`Album - ${JSON.stringify(albumForDelete)} successfully deleted!`)
     if (album) await this.dataSource.manager.delete(FavoritesAlbums, album);
     return await this.dataSource.manager.delete(Albums, { id: id });
   }
