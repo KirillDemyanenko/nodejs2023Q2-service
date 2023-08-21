72f833fd78ea9ac27711a923086733c0cbe08c14
KirillDemyanenko <demyanenkokg@gmail.com>
2023-08-21 19:30:03 +0300

    fix: remove swagger

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { SwaggerModule } from '@nestjs/swagger';
-import * as fs from 'fs';
-import * as YAML from 'js-yaml';
 import { LibraryLogger } from './logger/logger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     logger: new LibraryLogger(),
   });
-  const yamlFile = fs.readFileSync('doc/api.yaml', 'utf8');
-  const document = SwaggerModule.createDocument(app, YAML.load(yamlFile));
-  SwaggerModule.setup('doc', app, document);
   await app.listen(process.env.PORT || 4000);
 }
-bootstrap()
+bootstrap();
